433bcb68bb408868316aaf6ad86034c0dea92693
Julia <52479551+JuliaChernykh@users.noreply.github.com>
2023-04-30 18:18:48 +0200

    removed unused code

---
+++ src/app/test/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from './page.module.css'
+
+const Page: React.FC = () => {
+    return (
+        <div className={s.page}>Test page</div>
+    )
+}
+
+export default Page;
