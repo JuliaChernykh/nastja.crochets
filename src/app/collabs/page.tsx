67ace21e570665c1cbf3f57992381b73c706052f
Julia <52479551+JuliaChernykh@users.noreply.github.com>
2023-06-03 09:59:47 +0200

    pages templates

---
+++ src/app/collabs/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import s from "./page.module.css";
+
+const CollabsPage: React.FC = () => {
+  return <div className={s.page}>collabs page</div>;
+};
+
+export default CollabsPage;
